e5c71399cf4f87a140a4334cd437c7c380dd0dc5
Mercey <main.mercey@gmail.com>
2021-06-02 18:22:36 +0100

    Hero-scroll

--- js/apps.js
+++ js/apps.js
@@ -1,26 +1,31 @@
-var cards = $('#card-slider .slider-item').toArray();
+$(document).ready(function(){
+    var cards = $('#card-slider .slider-item').toArray();
 
-startAnim(cards);
+    startAnim(cards);
 
-function startAnim(array){
-    if(array.length >= 4 ) {
-        TweenMax.fromTo(array[0], 0.5, {x:0, y: 0, opacity:0.75}, {x:0, y: -220, opacity:0, zIndex: 0, delay:0.03, ease: Cubic.easeInOut, onComplete: sortArray(array)});
-
-        TweenMax.fromTo(array[1], 0.5, {x:79, y: 225, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.4, zIndex: 0, ease: Cubic.easeInOut});
-
-        TweenMax.to(array[2], 0.5, {bezier:[{x:0, y:350}, {x:65, y:300}, {x:79, y:225}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
-
-        TweenMax.fromTo(array[3], 0.5, {x:0, y:500, opacity: 0, zIndex: 0}, {x:0, y:450, opacity: 0.4, zIndex: 0, ease: Cubic.easeInOut}, );
-    } else {
-        $('#card-slider').append('<p>Sorry, carousel should contain more than 3 slides</p>')
+    function startAnim(array){
+        if(array.length >= 4 ) {
+            TweenMax.fromTo(array[0], 0.5, {x:0, y: 0, opacity:0.4}, {x:0, y: -220, opacity:0, zIndex: 0, delay:0.03, ease: Cubic.easeInOut, onComplete: sortArray(array)});
+            TweenMax.fromTo(array[1], 0.5, {x:79, y: 225, opacity:1, zIndex: 1}, {x:0, y: 0, opacity:0.4, zIndex: 0, ease: Cubic.easeInOut});
+            TweenMax.to(array[2], 0.5, {bezier:[{x:0, y:350}, {x:65, y:300}, {x:79, y:225}], zIndex: 1, opacity: 1, ease: Cubic.easeInOut});
+            TweenMax.fromTo(array[3], 0.5, {x:0, y:500, opacity: 0, zIndex: 0}, {x:0, y:450, opacity: 0.4, zIndex: 0, ease: Cubic.easeInOut}, );
+        } else {
+            $('#card-slider').append('<p>Sorry, carousel should contain more than 3 slides</p>')
+        }
     }
-}
 
-function sortArray(array) {
-    clearTimeout(delay);
-    var delay = setTimeout(function(){
-        var firstElem = array.shift();
-        array.push(firstElem);
-        return startAnim(array);
-    },3000)
-}
+    function sortArray(array) {
+        clearTimeout(delay);
+        var delay = setTimeout(function(){
+            var firstElem = array.shift();
+            array.push(firstElem);
+            return startAnim(array);
+        },2000)
+    }
+
+    $('.hero-scroll').click(function() {
+        $('html, body').animate({
+            scrollTop: $('#royale-section').offset().top
+        }, 1000);
+    });
+})
